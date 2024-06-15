6b29b1dcf029938ebb2d2f3deee1d01a246d2afd
sivaprasad2848 <sivaprasad@cybersquare.org>
2024-06-15 12:07:43 +0530

    Two Tables Declares in Models

--- cmsapp/models.py
+++ cmsapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name=models.CharField(max_length=50)
+    slug=models.CharField(max_length=50)
+    parent_id=models.IntegerField()
+
+class Posts(models.Model):
+    title=models.CharField(max_length=255)
+    content=models.TextField()
+    
